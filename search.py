cda5936b0689c6c71dd5171dc4af4aa50601a8fd
lorand <lorand77@users.noreply.github.com>
2024-10-01 18:27:38 +0200

    timing

--- search.py
+++ search.py
@@ -13,11 +13,17 @@
 # print(find([], 54))
 
 
+import time
+import random
+
 def is_even_and_gt_5(x):
     return x % 2 == 0 and x > 5
 
 def is_odd_and_gt_7(x):
     return x % 2 == 1 and x > 7
+
+def is_equal_10(x):
+    return x == 10
 
 def find(x, f):
     for i in range(len(x)):
@@ -26,7 +32,10 @@ def find(x, f):
     return None
 
 
-x = [43, 8, 29, 3, 8]
+x = list(range(10_000_000))
+random.shuffle(x)
 
-print(find(x, is_even_and_gt_5))
-print(find(x, is_odd_and_gt_7))
+start_time = time.time()
+ix = find(x, is_equal_10)
+end_time = time.time()
+print(ix, end_time - start_time)
