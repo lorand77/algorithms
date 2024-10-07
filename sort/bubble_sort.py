fa7dbb47242b03e60752294bbe0646f5677c81cc
lorand <lorand77@users.noreply.github.com>
2024-10-07 12:11:43 +0200

    bubble sort

---
+++ sort/bubble_sort.py
@@ -0,0 +1,19 @@
+import random
+
+def bubble_sort(x):
+    swaps = 0
+    reads = 0
+    for i in range(len(x) - 1, 0, - 1):
+        for j in range(0, i):
+            if x[j] > x[j + 1]:
+                x[j], x[j + 1] = x[j + 1], x[j]
+                swaps += 1
+            reads += 2
+    print(reads, swaps)
+
+
+x = list(range(1000))
+x.reverse()
+# random.shuffle(x)
+bubble_sort(x)
+# print(x)
