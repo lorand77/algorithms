15fa504fc29febdec5159650300ab2c4324504f6
lorand <lorand77@users.noreply.github.com>
2024-10-07 12:14:48 +0200

    debug

--- sort/bubble_sort.py
+++ sort/bubble_sort.py
@@ -1,6 +1,13 @@
 import random
 
 def bubble_sort(x):
+    for i in range(len(x) - 1, 0, - 1):
+        for j in range(0, i):
+            if x[j] > x[j + 1]:
+                x[j], x[j + 1] = x[j + 1], x[j]
+
+
+def bubble_sort_debug(x):
     swaps = 0
     reads = 0
     for i in range(len(x) - 1, 0, - 1):
@@ -15,5 +22,5 @@ def bubble_sort(x):
 x = list(range(1000))
 x.reverse()
 # random.shuffle(x)
-bubble_sort(x)
+bubble_sort_debug(x)
 # print(x)
